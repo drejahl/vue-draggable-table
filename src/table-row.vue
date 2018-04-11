9128efaa508bb9d300ea19d073bee61c0da47aed
Dirk Rejahl <dirk@digiglu.co.uk>
2018-04-11 11:37:49 +0100

    date formatting

--- src/table-row.vue
+++ src/table-row.vue
@@ -1,22 +1,36 @@
 <template>
   <tr>
     <template v-for="column in columns">
-      <td style="text-align: left;" v-show="!column.hide && column.type != 'icon' && !column.linkAway">
+      <td style="text-align: left;" v-show="!column.hide && column.type === 'string' && !column.linkAway">
+        {{row[column.id]}}
+      </td>
+      <td style="text-align: right;" v-show="!column.hide && column.type === 'number' && !column.linkAway">
         {{row[column.id]}}
       </td>
       <td style="text-align: left;" v-show="!column.hide && column.type != 'icon' && column.linkAway">
         <router-link :to="row[column.linkAway]">{{row[column.id]}}</router-link>
       </td>
-      <td style="text-align: left;" v-show="!column.hide && column.type === 'icon'  && !column.linkAway">
+      <td style="text-align: left;" v-if="!column.hide && column.type === 'icon'  && !column.linkAway">
         <i class="icon material-icons">{{row[column.id]}}</i>
+      </td>
+      <td style="text-align: left;" v-if="!column.hide && column.type === 'date'">
+        {{formatedDate(row[column.id], column.dateFormat)}}
       </td>
     </template>
   </tr>
 </template>
 
 <script>
+import fecha from 'fecha'
+
 export default {
   name: 'table-row',
-  props: ["row", "columns"]
+  props: ["row", "columns"],
+  methods: {
+    formatedDate: function(d,f){
+      console.log("DATE", d)
+      return fecha.format( parseInt(d),f);
+    }
+  }
 }
 </script>
